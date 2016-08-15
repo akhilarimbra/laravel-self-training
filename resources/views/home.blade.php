116db814b0b2487f9546c646b5c272787a725456
Akhil Raj <nilamburakhil@gmail.com>
2016-08-15 06:54:44 +0000

    chapter 3

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('title')
+Learning Laravel PHP framework for web artisans
+@endsection
+@section('content')
+    <div class="centered">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/blog">Blog</a>
+    </div>
+@endsection
